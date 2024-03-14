d0bbc1e09bb0e0af9cd9d09c26bdf0a255c30e31
JuanCa <juanca_20201@outlook.es>
2024-03-13 22:09:02 -0500

    Ver Mas Heroes y Buscador obsoleto

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from 'src/environments/environments';
 
@@ -15,5 +15,20 @@ export class HeroesService {
 
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${ this.baseUrl }/heroes`);
+  }
+
+  getHeroById( id: string ): Observable<Hero | undefined> {
+    return this.http.get<Hero>(`${ this.baseUrl }/heroes/${ id }`)
+    .pipe(
+      catchError( error => of(undefined) )
+    );
+  }
+
+  getSuggestions2( query: string ): Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${ this.baseUrl }/heroes?_start=${ query }&_limit=6`);
+  }
+
+  getSuggestions( query: string ): Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${ this.baseUrl }/heroes?q=${ query }&_limit=6`);
   }
 }
